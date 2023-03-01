82e37f81d2b7f6461e01c2468380a99fc8edd399
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2023-03-01 21:05:57 +0300

    adding functionalityto Blog.js

--- src/components/home/Blog.js
+++ src/components/home/Blog.js
@@ -6,45 +6,71 @@ import {
   CiSquareChevRight,
   CiEdit,
 } from "react-icons/ci";
+import { ImCheckmark, ImCross } from "react-icons/im";
 
 // react-router-dom
 import { Link } from "react-router-dom";
 
+// react
 import React from "react";
 import { useState } from "react";
 
 export default function Blog({ blog }) {
+  const [editableBlog, setEditableBlog] = useState(blog);
   const [isEditable, setIsEditable] = useState(false);
+  const [newTitle, setNewTitle] = useState("");
 
   function handleEdit() {
     setIsEditable(true);
+  }
+
+  function handleSave() {
+    setEditableBlog((prevBlog) => {
+      return {
+        ...prevBlog,
+        title: newTitle,
+      };
+    });
+
+    setIsEditable(false);
   }
 
   return (
     <div className={Styles.blogCard}>
       <header>
         <div className={Styles.titleContainer}>
-          <Link to={`/single/${blog.id}`} key={blog?.id}>
-            <h2>
-              <CiSquareChevRight className={Styles.icon} />
+          <h2>
+            <CiSquareChevRight className={Styles.icon} />
 
-              {isEditable ? (
-                <input type="text" defaultValue={blog.title} />
-              ) : (
-                <>{blog?.title}</>
-              )}
-            </h2>
-          </Link>
-
-          <CiEdit className={Styles.iconEditTitle} onClick={handleEdit} />
+            {isEditable ? (
+              <div className={Styles.editTitle}>
+                <input
+                  type="text"
+                  defaultValue={editableBlog?.title}
+                  onChange={(e) => setNewTitle(e.target.value)}
+                />
+                <span>
+                  <ImCheckmark onClick={handleSave} />
+                  <ImCross onClick={() => setIsEditable(false)} />
+                </span>
+              </div>
+            ) : (
+              <>
+                <Link to={`/single/${editableBlog?.id}`} key={editableBlog?.id}>
+                  {editableBlog?.title}
+                </Link>
+                <CiEdit className={Styles.iconEditTitle} onClick={handleEdit} />
+              </>
+            )}
+          </h2>
         </div>
 
         <h3>
-          <CiUser className={Styles.icon} /> {blog?.author}
+          <CiUser className={Styles.icon} /> {editableBlog?.author}
         </h3>
       </header>
       <p>
-        <CiStickyNote className={Styles.iconPara} /> {blog?.body}
+        <CiStickyNote className={Styles.iconPara} /> {editableBlog?.body}
       </p>
     </div>
   );
