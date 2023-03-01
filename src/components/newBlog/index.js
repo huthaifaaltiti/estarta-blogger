230c91b362b59f5c7e0e5e71d018afc8bd9b1425
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2023-03-01 19:46:45 +0300

    editing index.js for addBlog comp

--- src/components/newBlog/index.js
+++ src/components/newBlog/index.js
@@ -2,27 +2,73 @@
 import "../../index.css";
 import Styles from "./styles.module.css";
 
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
 const NewBlog = () => {
+  const navigator = useNavigate();
+
+  const myRef = useRef({
+    title: "",
+    body: "",
+    author: "",
+  });
+
+  function handleChangeForm(e) {
+    myRef.current = {
+      ...myRef.current,
+      [e.target.name]: e.target.value,
+    };
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    // Note: form behavior when click "Submit" will make page-reload, so use the line before.
+    await fetch("http://localhost:7000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(myRef.current),
+    });
+
+    navigator("/");
+  }
+
   return (
     <div className={Styles.page}>
       <div className={Styles.newBlog}>
         <header>
           <h2>Add a new blog</h2>
         </header>
-        <form>
+        <form onSubmit={handleSubmit}>
+          {/* Note: handleSubmit is equal to (e) => handleSubmit(e) */}
           <label>
             Blog title
-            <input type="text" name="name" placeholder="Blog Title" />
+            <input
+              type="text"
+              name="title"
+              placeholder="Blog Title"
+              onChange={handleChangeForm}
+            />
           </label>
 
           <label>
             Blog Author
-            <input type="text" name="name" placeholder="Blogger Name" />
+            <input
+              type="text"
+              name="author"
+              placeholder="Blogger Name"
+              onChange={handleChangeForm}
+            />
           </label>
 
           <label>
             Blog Body
-            <textarea type="text" name="name" placeholder="Write a blog" />
+            <textarea
+              type="text"
+              name="body"
+              placeholder="Write a blog"
+              onChange={handleChangeForm}
+            />
           </label>
 
           <div className={Styles.formSubmit}>
